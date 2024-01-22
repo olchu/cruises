868fdb6e68aaafd0e9b04a887906340b202c4cb8
Oleg Churkin <olchu30@yandex.ru>
2024-01-22 22:00:06 +0300

    fix cookieBanner

---
+++ src/shared/constants/pagesLink.ts
@@ -0,0 +1,4 @@
+export const pagesLink = {
+  privacy: '/privacy',
+  contacts: '/contacts',
+};
--- src/widgets/cookieBanner/ui/CookieBanner.tsx
+++ src/widgets/cookieBanner/ui/CookieBanner.tsx
@@ -1,16 +1,20 @@
+import { pagesLink } from '@/shared/constants/pagesLink';
 import { getCookieByName } from '@/shared/lib/utils/getCookieByName';
 import { MainContainer } from '@/shared/ui/mainContainer/MainContainer';
-import { Heading, HStack, Text, Button, VStack } from '@chakra-ui/react';
+import { Heading, HStack, Text, Button, VStack, Link } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import NextLink from 'next/link';
 import { useEffect, useState } from 'react';
 
 const COOKIE_BANNER_NAME = 'cookieBanner';
+
+const MotionHStack = motion(HStack);
 
 export const CookieBanner = () => {
   const [isShow, setIsShow] = useState(false);
 
   useEffect(() => {
     const cookie = getCookieByName(COOKIE_BANNER_NAME);
-    console.log('cookie', cookie);
     if (cookie !== 'false') {
       setIsShow(true);
     }
@@ -21,31 +25,52 @@ export const CookieBanner = () => {
     setIsShow(false);
   };
 
-  if (!isShow) return null;
+  const bannerVariants = {
+    hidden: { bottom: -140 },
+    visible: { bottom: 0 },
+  };
+
   return (
-    <HStack position="absolute" w="full" bg="lightBlue" bottom={0}>
-      <MainContainer
-        as="div"
-        overflow="hidden"
-        px={{ base: 'section.mobile', lg: 'section.desktop' }}
-        py={{ base: 'section.mobile', lg: 'section.desktop' }}
-        maxW={'1400px'}
-        h="full"
-      >
-        <HStack gap="20px" justifyContent="flex-end">
-          <VStack gap="12px" alignItems="flex-start">
-            <Heading fontSize="18px">Мы используем файлы cookie.</Heading>
-            <Text>
-              Некоторые функции могут не работать, если не установлены cookies.
-              Чтобы узнать больше об использовании cookies на нашем сайте,
-              перейдите на
-            </Text>
-          </VStack>
-          <Button bg="primary" color="white" onClick={handleClose}>
-            Закрыть
-          </Button>
-        </HStack>
-      </MainContainer>
-    </HStack>
+    <AnimatePresence>
+      {isShow && (
+        <MotionHStack
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={bannerVariants}
+          transition={{ duration: 0.6, type: 'spring' }}
+          position="fixed"
+          w="full"
+          bg="lightBlue"
+          bottom={0}
+        >
+          <MainContainer
+            as="div"
+            overflow="hidden"
+            px={{ base: 'section.mobile', lg: 'section.desktop' }}
+            py={{ base: 'section.mobile', lg: 'section.desktop' }}
+            maxW={'1400px'}
+            h="full"
+          >
+            <HStack gap="20px" justifyContent="flex-start">
+              <VStack gap="12px" alignItems="flex-start">
+                <Heading fontSize="18px">Мы используем файлы cookie.</Heading>
+                <Text>
+                  Некоторые функции могут не работать, если не установлены
+                  cookies. Чтобы узнать больше об использовании cookies на нашем
+                  сайте, перейдите на{' '}
+                  <Link as={NextLink} href={pagesLink.privacy} color="primary">
+                    информационную страницу Cookies
+                  </Link>
+                </Text>
+              </VStack>
+              <Button bg="primary" color="white" onClick={handleClose}>
+                Закрыть
+              </Button>
+            </HStack>
+          </MainContainer>
+        </MotionHStack>
+      )}
+    </AnimatePresence>
   );
 };
