6596da955df12183d64c2b85fc58c466aefaacd5
Oleg Churkin <olchu30@yandex.ru>
2024-03-17 12:10:49 +0300

    fix pages compilation

--- src/pages/pages/[slug].tsx
+++ src/pages/pages/[slug].tsx
@@ -12,7 +12,15 @@ import Head from 'next/head';
 import { itemsOnPage } from '@/shared/constants/constants';
 import { SearchResultContent } from '@/features/searchResultContent';
 import Image from 'next/image';
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react';
 
 export type CompilationPageProps = {
   compilation: PagesPrismaType;
@@ -25,7 +33,7 @@ const CompilationPage = ({ compilation }: CompilationPageProps) => {
   const [skip, setSkip] = useState(0);
   const [cruisesCount, setCruisesCount] = useState(0);
   const metaTags = compilation.metaTag as TagPrismaType[];
-  const queries = (compilation.query as string[]);
+  const queries = compilation.query as string[];
 
   const getCruises = useCallback(async () => {
     if (!queries) {
@@ -74,17 +82,46 @@ const CompilationPage = ({ compilation }: CompilationPageProps) => {
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box w="full" h="250px" overflow="hidden" position="relative">
+      <Box
+        w="full"
+        h={{ base: '150px', lg: '250px' }}
+        minH={{ base: '150px', lg: '250px' }}
+        overflow="hidden"
+        position="relative"
+      >
         <Image
           src={compilation?.images || '/temp.jpg'}
           alt={compilation.title || ''}
           fill={true}
           style={{ objectFit: 'cover' }}
         />
+        <Box
+          as="span"
+          position="absolute"
+          px={{ base: 'section.mobile', lg: 'section.desktop' }}
+          py={{ base: 'section.mobile', lg: 'section.desktop' }}
+          bottom="0"
+          left="0"
+          bg="linear-gradient(0deg, rgba(0,0,0,0.8057598039215687) 40%, rgba(255,255,255,0) 100%)"
+          w="full"
+          h="45%"
+        />
         {compilation.title && (
-          <Text position="absolute" bottom="0" left="0">
-            {compilation.title}
-          </Text>
+          <MainContainer as="div" maxW={'1400px'} position="relative" h="full">
+            <Text
+              position="absolute"
+              px={{ base: 'section.mobile', lg: 'section.desktop' }}
+              py={{ base: 'section.mobile', lg: 'section.desktop' }}
+              bottom="0"
+              left="0"
+              color="white"
+              fontSize="26px"
+              fontWeight="bold"
+              w="full"
+            >
+              {compilation.title}
+            </Text>
+          </MainContainer>
         )}
       </Box>
       <MainContainer
@@ -95,15 +132,37 @@ const CompilationPage = ({ compilation }: CompilationPageProps) => {
         maxW={'1400px'}
         h="full"
       >
-        {compilation.query && (
-          <SearchResultContent
-            handleGetMore={handleGetMore}
-            isLoading={isLoading}
-            isFetching={isFetching}
-            cruises={cruises}
-            cruisesCount={cruisesCount}
-          />
-        )}
+        <Tabs>
+          <TabList mb="12px">
+            <Tab fontSize="18px" fontWeight="bold" color="primary">
+              Круизы
+            </Tab>
+            {compilation?.content && <Tab fontSize="18px" fontWeight="bold" color="primary">Описание</Tab>}
+          </TabList>
+
+          <TabPanels>
+            <TabPanel p="0">
+              {compilation.query && (
+                <SearchResultContent
+                  handleGetMore={handleGetMore}
+                  isLoading={isLoading}
+                  isFetching={isFetching}
+                  cruises={cruises}
+                  cruisesCount={cruisesCount}
+                />
+              )}
+            </TabPanel>
+            {compilation?.content && (
+              <TabPanel>
+                <Box
+                  dangerouslySetInnerHTML={{
+                    __html: compilation?.content || '',
+                  }}
+                />
+              </TabPanel>
+            )}
+          </TabPanels>
+        </Tabs>
       </MainContainer>
     </>
   );
