218ed26c263c5e426057cc34377e965943b7da7f
Oleg Churkin <olchu30@yandex.ru>
2024-03-31 22:40:29 +0300

    image path fix

--- src/pages/api/admin/syncShips.ts
+++ src/pages/api/admin/syncShips.ts
@@ -51,11 +51,11 @@ export default async function handler(
           where: {
             id: selectShip?.id,
           },
-          data: { ...ship, img: `/${imgPath}` },
+          data: { ...ship, img: `https://new.vbp.ru/${imgPath}` },
         });
       } else {
         const createRes = await prisma.ships.create({
-          data: { ...ship, img: `/${imgPath}` },
+          data: { ...ship, img: `https://new.vbp.ru/${imgPath}` },
         });
       }
 
