df2329b5597e423e6e7b1ea129d60d008dda16af
Oleg Churkin <olchu30@yandex.ru>
2024-08-21 20:44:52 +0300

    rm unused comp

--- src/pages/admin/news.tsx
+++ src/pages/admin/news.tsx
@@ -1,7 +1,5 @@
 import { AddNewsForm } from '@/features/admin/addNews';
-import { AddPostForm } from '@/features/admin/addPost';
 import AdminLayout from '@/layouts/admin';
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
 const News = () => {
